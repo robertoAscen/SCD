2596ddc9768db12e919ef95e5c090c30ad119dae
Roberto Ascencio <roberto.ascencio.7@gmail.com>
2017-12-03 12:37:35 -0600

    Primer Commit del proyecto

---
+++ app/src/main/java/com/example/rascencio/self_care_diabetics/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rascencio.self_care_diabetics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
